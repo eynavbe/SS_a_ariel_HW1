6bbfb842be43832bc80f333bab6bcb68f93136f3
eynavbe <93534494+eynavbe@users.noreply.github.com>
2021-11-02 23:20:57 +0200

    Add files via upload

---
+++ NumClass.h
@@ -0,0 +1,38 @@
+/* will return if a number is Armstrong number
+An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
+For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
+*/
+int isArmstrong(int);
+
+/* will return if a number is a palindrome */
+int isPalindrome(int);
+
+/* will return if a number is prime*/
+int isPrime(int);
+
+/*Strong number is a special number whose sum of the factorial of digits is equal to the original
+number. For Example: 145 is strong number
+1! + 4! + 5! = 145
+*/
+int isStrong(int);
+
+/* will return number power counts digits get number and counts the amount of digits (in Recursion)*/
+int isArmstrongAz(int,int);
+
+/* Will return an sum so that ArmstrongAz gets all the digits of the number (in Recursion)*/
+int isArmstrongSum(int,int,int);
+
+/* will return counts the amount of digits get number*/
+int countDigits(int);
+
+/*Calculating an assembly of a number (in Loop)*/
+int calculationAssembly(int);
+
+/* will return reverse number in array get count digits and number (in Loop)*/
+char * intToChar(int, int);
+
+/* will return reverse number get count digits and number (in Recursion)*/
+int reverseNumber (int, int );
+
+/* will return 10 power counts the amount of digits -1 (in Recursion)*/
+int zeroDigits(int );
