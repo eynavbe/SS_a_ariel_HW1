fd646b5fcf757cd57e797e3d58b13968d04a8f65
eynavbe <93534494+eynavbe@users.noreply.github.com>
2021-11-03 17:34:04 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "NumClass.h"
+int main()
+{
+    int min;
+    int a;
+    int max;
+    printf("Write 2 positive integers: ");
+    scanf("%d %d", &a, &max);
+    if (a > max)
+    {
+        min = max;
+        max = a;
+    }else{
+        min = a;
+    }
+    printf("\nThe Armstrong numbers are:");
+    for (int i = min; i <= max; i++)
+    {
+        if(isArmstrong(i))
+        {
+        printf(" %d",i);
+        }
+    }
+    printf("\nThe Palindromes are:");
+    for (int i = min; i <= max; i++)
+    {
+        if(isPalindrome(i))
+        {
+        printf(" %d",i);
+        }
+    }
+    printf("\nThe Prime numbers are:");
+    for (int i = min; i <= max; i++)
+    {
+        if(isPrime(i))
+        {
+        printf(" %d",i);
+        }
+    }
+    printf("\nThe Strong numbers are:");
+    for (int i = min; i <= max; i++)
+    {
+        if(isStrong(i))
+        {
+        printf(" %d",i);
+        }
+    }
+    printf("\n");
+}
